9486bee6fd0c37af9e3784a1f7961ce70b19af97
Winner107 <154951453+Winner107@users.noreply.github.com>
2024-09-02 15:22:24 -0500

    Update Hotel Data Visualization.R

--- Hotel Data Visualization.R
+++ Hotel Data Visualization.R
@@ -21,12 +21,12 @@ ggplot(data = hotel_bookings) +
 ggplot(data = hotel_bookings) +
   geom_bar(mapping = aes(x=distribution_channel,fill=deposit_type)) + facet_wrap(~deposit_type) +
   theme(axis.text.x = element_text(angle = 45))
-ggsave('Hotel 2.png')
+ggsave('Hotel_2.png')
 
 ggplot(data = hotel_bookings) +
   geom_bar(mapping = aes(x=distribution_channel,fill=market_segment)) + facet_wrap(~market_segment) +
   theme(axis.text.x = element_text(angle = 45))
-ggsave('Hotel 1.png')
+ggsave('Hotel_1.png')
 
 ggplot(data = hotel_bookings) +
   geom_bar(mapping = aes(x=distribution_channel,fill=market_segment)) + facet_grid(deposit_type~market_segment) +
@@ -36,4 +36,4 @@ ggplot(data = hotel_bookings) +
   geom_bar(mapping = aes(x=distribution_channel), fill='purple') + facet_grid(deposit_type~market_segment) +
   theme(axis.text.x = element_text(angle = 45))
 
-ggsave('Hotel Booking Idea.png')
+ggsave('Hotel_Booking_Idea.png')
